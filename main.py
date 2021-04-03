702c9543b5f3a0e7cdda9d372110e7e1a2659561
aditya2604 <aditya26042005@gmail.com>
2021-04-02 19:31:46 -0700

    set default window size

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.widget import Widget
+from kivy.config import Config
+
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '576')
+
+
+class Ball(Widget):
+    pass
+
+
+class Obstacle(Widget):
+    pass
 
 
 class BallRunner(Widget):
