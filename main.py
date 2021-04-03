0bd489c7981b651a9ab7bf2faf97aa3e09284c2b
aditya2604 <aditya26042005@gmail.com>
2021-04-02 18:59:34 -0700

    working rectangle

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.widget import Widget
 
 
-class BallRunnerGame(Widget):
+class BallRunner(Widget):
     pass
 
 
 class BallRunnerApp(App):
+
     def build(self):
-        return BallRunnerGame()
+        return BallRunner()
 
 
-BallRunnerApp().run()
+if __name__ == "__main__":
+    window = BallRunnerApp()
+    window.run()
