c2da8fa75b15ccbf663d46ec34b4ec4d4d63a8c5
aditya2604 <aditya26042005@gmail.com>
2021-04-02 17:41:42 -0700

    basic canvas

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import kivy
-print("Hello World")
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class BallRunnerGame(Widget):
+    pass
+
+
+class BallRunnerApp(App):
+    def build(self):
+        return BallRunnerGame()
+
+
+BallRunnerApp().run()
