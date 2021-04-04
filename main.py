6605277849491664f9b04f29a1b6243f04300482
aditya2604 <aditya26042005@gmail.com>
2021-04-03 23:37:03 -0700

    added background movement

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.config import Config
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.uix.image import Image
 from kivy.graphics import *
 
 def_x_obs = 1300
@@ -16,6 +16,32 @@ def_y_ball = 200
 
 obs_right_end = -160
 ball_max_height = 800
+
+
+class Background(Widget):
+    grass_texture = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.size = Window.size
+        self.pos = (0, 0)
+
+        # Create textures
+        self.grass_texture = Image(source="images/grass.jpg").texture
+        self.grass_texture.wrap = 'repeat'
+        self.grass_texture.uvsize = (Window.width / self.grass_texture.width, -1)
+        self.time_passed = 0
+
+    def scroll_textures(self, time_passed):
+        # Update the uvpos of the texture
+        self.time_passed = time_passed
+        self.grass_texture.uvpos = ((self.grass_texture.uvpos[0] + self.time_passed/2.0) % Window.width, self.grass_texture.uvpos[1])
+        print(self.time_passed)
+
+        # Redraw the texture
+        texture = self.property('grass_texture')
+        texture.dispatch(self)
 
 
 class Ball(Widget):
@@ -43,13 +69,14 @@ class Obstacle(Widget):
 
 
 # moving the obstacle by calling move()
-class BallRunner(Widget):
+class BallRunner(Background, Widget):
     obstacle = ObjectProperty(None)
     ball = ObjectProperty(None)
     event = None
     current_touch = None
     img = None
     obs_start_spd = -7.0
+    ball_start_down_spd = -8
 
     def moving_obstacle(self):
         self.obstacle.velocity = Vector(self.obs_start_spd, 0)
@@ -64,18 +91,23 @@ class BallRunner(Widget):
             self.obstacle.y = def_y_obs
             self.obs_start_spd -= .5
             self.obstacle.velocity = Vector(self.obs_start_spd, 0)
+            self.ball_start_down_spd -= .2
+            self.ball.velocity = Vector(0, self.ball_start_down_spd)
+            self.time_passed += .5
+            print(self.time_passed)
 
         if self.obstacle.has_collided(self.ball):
             try:
                 self.event.cancel()
             except:
                 pass
+            self.grass_texture.uvpos = (0, 0)
             self.obstacle.velocity = Vector(0, 0)
             with self.canvas:
                 explosion = Rectangle(source="images/explosion.png", pos=(self.ball.x-self.ball.width, self.ball.y-self.ball.height), size=(250, 250))
 
     def moving_ball(self):
-        self.ball.velocity = Vector(0, 10)
+        self.ball.velocity = Vector(0, 11)
 
     def update_ball(self, dt):
         self.ball.move()
@@ -83,7 +115,7 @@ class BallRunner(Widget):
         # checks if ball has reached climax and changes directions if it has
 
         if self.ball.y >= ball_max_height:
-            self.ball.velocity = Vector(0, -9)
+            self.ball.velocity = Vector(0, self.ball_start_down_spd)
 
         # checks if ball has reached to base and stops if it has
 
@@ -102,12 +134,13 @@ class BallRunner(Widget):
 
 
 class BallRunnerApp(App):
-    obstacle_event = None
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids._background.scroll_textures, 1/120.0)
 
     def build(self):
         game = BallRunner()
         game.moving_obstacle()
-        Clock.schedule_interval(game.update_obstacle, 1.0/60.0)
+        Clock.schedule_interval(game.update_obstacle, 1/360.0)
         return game
 
 
